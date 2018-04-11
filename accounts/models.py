9e7f960604eef5671967efb2d65b3d594527467f
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-04-11 21:20:51 +0100

    Extended user model using Profile model which has an option to add a child/children as a foreign key.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Child(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    surname = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    child = models.ForeignKey(Child,
+                              null=True,
+                              on_delete=models.CASCADE,
+                              related_name="parent")
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
