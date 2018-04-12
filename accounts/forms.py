c8529d22e0fe9995e0c4e626b4e942bfc40ee1de
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-04-12 20:38:16 +0100

    Created core/form.html for login and sign up using django-bootstrap4 library (to be used - {% include with title="title name" %}). Changed default password help text. Added Required as help text to Email field. Styled form classes globally.

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,9 +7,14 @@ class UserCreateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['email'].required = True
+        self.fields['password1'].help_text = '<ul><li>Minimum 8 characters long.\n</li><li>Cannot be numerical only.\n</li><li>Cannot be similar to your personal data.\n</li><li>Common words are not allowed.</li></ul>'
 
     class Meta:
         model = get_user_model()
 
         fields = ('username', 'email', 'password1', 'password2')
+        help_texts = {
+            'email': 'Required',
+        }
+
 
