771057d3cc873c13e01dfa6fa53d19b03df2a5c5
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-04-29 11:19:13 +0100

    Removed awkward pointer events from list-group-items (borders were disappearing on hover). Created include: _action_list to display list of smileys and oopsies and implemented in child_detail.html. Added a new filter tag: get_model_name to retrieve model's name inside the template.

--- dashboard/templatetags/dashboard_tags.py
+++ dashboard/templatetags/dashboard_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def set_var(variable):
     return variable
+
+
+@register.filter
+def get_model_name(value):
+    return value.__class__.__name__
