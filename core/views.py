58128aad33ca5869863ecad47eb6ff1e00d9e761
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-05-03 15:00:50 +0100

    Changed star award style. Added child to context data for homepage if logged in user is child.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import TemplateView
+from accounts.models import Child
 
 # Create your views here.
 
 
 class HomePage(TemplateView):
+
     template_name = "core/index.html"
+
+    def get_context_data(self, **kwargs):
+        if self.request.user.is_authenticated and self.request.user.is_child:
+            print("user is a child")
+            kwargs['parent_username'] = Child.objects.get(user=self.request.user).parent.username
+            print(kwargs['parent_username'])
+        return super().get_context_data(**kwargs)
