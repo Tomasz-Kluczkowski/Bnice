54fbfd0091141c8aa6d43c0af2c5eb123c8fa723
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-05 21:05:37 +0100

    Test AddSmileyForm.
    Added fixture to generate a valid AddSmileyForm.
    Added test of a valid AddSmileyForm.

--- dashboard/tests/test_forms.py
+++ dashboard/tests/test_forms.py
@@ -1,1 +1,31 @@
 import pytest
+
+from dashboard.forms import AddSmileyForm, AddOopsyForm
+
+
+@pytest.fixture
+def add_smiley_form_with_set_description():
+    """Returns an instance of AddSmileyForm with a description from one of the
+    choices."""
+    form = AddSmileyForm({'description': 'Folded washing',
+                          'new_description': '',
+                          'points': 3})
+    return form
+
+
+@pytest.mark.django_db
+def test_add_smiley_form_with_valid_data(add_smiley_form_with_set_description):
+    form = add_smiley_form_with_set_description
+    assert form.is_valid() is True
+    # Confirm manually set field attributes are ok.
+    assert form.fields['description'].help_text == 'Required'
+    assert form.fields['new_description'].help_text == (
+        'Required, Create a new description')
+    assert form.fields['points'].help_text == (
+        'Required, How much was this task worth?')
+
+@pytest.mark.django_db
+def
+
+
+
