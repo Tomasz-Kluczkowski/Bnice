921db0abae0b31b7661a3dec755b9c63f0294492
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-14 19:48:01 +0100

    Add tests for dashboard tags.
    Added tests for set_var and get_model_name tags.

---
+++ dashboard/tests/test_dashboard_tags.py
@@ -0,0 +1,11 @@
+import pytest
+
+from dashboard.templatetags.dashboard_tags import set_var, get_model_name
+
+
+def test_set_var_tag():
+    assert set_var('test_value') == 'test_value'
+
+
+def test_get_model_name(smiley_custom_description):
+    assert get_model_name(smiley_custom_description) == 'Smiley'
