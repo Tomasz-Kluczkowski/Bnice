55eab4459882c2e574fa05cd47871d5d69387fba
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-14 20:03:44 +0100

    Comment what to test next.
    Added comments for further test.

--- dashboard/tests/test_views.py
+++ dashboard/tests/test_views.py
@@ -4,6 +4,7 @@ from dashboard.views import DashboardPage
 from django.urls import reverse
 
 pytestmark = pytest.mark.django_db
-
+# here we need to find out how to log in a client and access
+# child_list for them from request object.
 
 
