394cdfe1f8a3a5c5ac0dfeaaa0c806846eb466ab
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-14 19:40:32 +0100

    Add missing dashboard/models.py tests.
    Increased models.py coverage to 100%.
    Added tests for reverse url and __str__ methods to action like objects
    (Smiley and Oopsy).

--- dashboard/tests/test_models.py
+++ dashboard/tests/test_models.py
@@ -1,1 +1,23 @@
 import pytest
+
+
+def test_smiley_reverse(smiley_custom_description):
+    """Confirm correct reverse url."""
+    smiley = smiley_custom_description
+    assert smiley.get_absolute_url() == '/dashboard/child/detail/tom_k/nat_k/1'
+
+
+def test_oopsy_reverse(oopsy_custom_description):
+    """Confirm correct reverse url."""
+    oopsy = oopsy_custom_description
+    assert oopsy.get_absolute_url() == '/dashboard/child/detail/tom_k/nat_k/1'
+
+
+def test_smiley_str(smiley_custom_description):
+    smiley = smiley_custom_description
+    assert smiley.__str__() == 'Removed rubbish'
+    
+
+def test_oopsy_str(oopsy_custom_description):
+    oopsy = oopsy_custom_description
+    assert oopsy.__str__() == 'Was rude'
