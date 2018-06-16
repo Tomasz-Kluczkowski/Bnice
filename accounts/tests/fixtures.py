4d4d8b82a4df1cb501dd51713fb872e9d75e3654
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-16 12:35:46 +0100

    Reconfigure tests.
    Moved fixtures to separate fixture.py files for accounts and dashboard
    tests. Now fixtures are added as pytest plugin.
    This allows better modularity and avoids showing unused imports.

---
+++ accounts/tests/fixtures.py
@@ -0,0 +1,51 @@
+import pytest
+from accounts.tests.factories import UserFactory, ChildFactory
+
+
+@pytest.fixture()
+def parent_user(db):
+    """Creates a parent website user."""
+    user = UserFactory(username='tom_k', name='Tom',
+                       email='tom@dot.pl', is_parent=True,
+                       is_child=False, profile_photo='')
+    return user
+
+
+@pytest.fixture()
+def parent_user_password(db, parent_user):
+    """Creates a parent website user with password."""
+    user = parent_user
+    user.set_password('password')
+    user.save()
+
+
+@pytest.fixture()
+def alt_parent_user_password(db):
+    """Creates an alt parent website user with password.
+
+    This will be used as an alternative parent user to confirm only children
+    of currently logged in parent are shown in views.
+    """
+
+    user = UserFactory(username='johny_c', name='Johny',
+                       email='johny@dot.pl', is_parent=True,
+                       is_child=False, profile_photo='')
+    user.set_password('password')
+    user.save()
+
+
+@pytest.fixture()
+def child_user(db):
+    """Creates a child website user."""
+    child_user = UserFactory(username='nat_k', name='Natalie',
+                             email='nat@dot.pl', is_parent=False,
+                             is_child=True, profile_photo='')
+    return child_user
+
+
+@pytest.fixture()
+def child(db, child_user, parent_user):
+    """Creates a Child object in the database."""
+    child_obj = ChildFactory(user=child_user, parent=parent_user,
+                             star_points=15)
+    return child_obj
---
+++ dashboard/tests/fixtures.py
@@ -0,0 +1,116 @@
+import pytest
+from django.utils import timezone
+
+from dashboard.forms import AddSmileyForm, AddOopsyForm
+from dashboard.tests.factories import SmileyFactory, OopsyFactory
+
+
+@pytest.fixture
+def add_smiley_form_set_description():
+    """Returns an instance of AddSmileyForm with a description from one of the
+    choices."""
+    form = AddSmileyForm({'description': 'Folded washing',
+                          'new_description': '',
+                          'points': 3})
+    return form
+
+
+@pytest.fixture
+def add_smiley_form_new_description():
+    """Returns an instance of AddSmileyForm with a new, custom description."""
+    form = AddSmileyForm({'description': 'Add new',
+                          'new_description': 'New description',
+                          'points': 3})
+    return form
+
+
+@pytest.fixture
+def smiley_form_new_description_saved(add_smiley_form_new_description, child):
+    """Returns a saved instance of the form with a new description."""
+    form = add_smiley_form_new_description
+    obj = form.save(commit=False)
+    obj.owner = child
+    current_time = timezone.now()
+    obj.earned_on = current_time
+    # Save object to the database
+    form.save()
+    return form
+
+
+@pytest.fixture
+def smiley_custom_description(child):
+    """Return a Smiley object with a custom description."""
+    custom_smiley = SmileyFactory(owner=child,
+                                  earned_on=timezone.now(),
+                                  description="Removed rubbish",
+                                  points=5)
+    custom_smiley.save()
+    return custom_smiley
+
+
+@pytest.fixture
+def smileys_with_same_description(child):
+    # Create 5 Smiley objects with the same description.
+    for i in range(5):
+        custom_smiley = SmileyFactory(owner=child,
+                                      earned_on=timezone.now(),
+                                      description="Removed rubbish",
+                                      points=5)
+        custom_smiley.save()
+
+
+# Oopsy form fixtures.
+
+
+@pytest.fixture
+def add_oopsy_form_set_description():
+    """Returns an instance of AddSmileyForm with a description from one of the
+    choices."""
+    form = AddOopsyForm({'description': 'Left mess',
+                         'new_description': '',
+                         'points': 3})
+    return form
+
+
+@pytest.fixture
+def add_oopsy_form_new_description():
+    """Returns an instance of AddOopsyForm with a new, custom description."""
+    form = AddOopsyForm({'description': 'Add new',
+                         'new_description': 'New description',
+                         'points': 3})
+    return form
+
+
+@pytest.fixture
+def oopsy_form_new_description_saved(add_oopsy_form_new_description, child):
+    """Returns a saved instance of the form with a new description."""
+    form = add_oopsy_form_new_description
+    obj = form.save(commit=False)
+    obj.owner = child
+    current_time = timezone.now()
+    obj.earned_on = current_time
+    # Save object to the database
+    form.save()
+    return form
+
+
+@pytest.fixture
+def oopsy_custom_description(child):
+    """Return an Oopsy object with a custom description."""
+    custom_oopsy = OopsyFactory(owner=child,
+                                earned_on=timezone.now(),
+                                description='Was rude',
+                                points=5)
+    custom_oopsy.save()
+    return custom_oopsy
+
+
+@pytest.fixture
+def oopsies_with_same_description(child):
+    """Create 5 Oopsy objects with the same description."""
+    for i in range(5):
+        custom_oopsy = OopsyFactory(owner=child,
+                                    earned_on=timezone.now(),
+                                    description='Was rude',
+                                    points=5)
+        custom_oopsy.save()
