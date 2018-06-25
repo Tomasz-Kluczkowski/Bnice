f53496890512dfe4722aaa6675949aa1f9f90319
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-25 16:23:50 +0100

    Add user_logger function to allow easier logging of users.
    Added user_logger function which simplifies logging of users in tests.

--- dashboard/tests/test_views.py
+++ dashboard/tests/test_views.py
@@ -7,15 +7,18 @@ from accounts.models import User, Child
 # Mark all tests as requiring database.
 pytestmark = pytest.mark.django_db
 
+
+def user_logger(client, username, password='password'):
+    client.login(username=username, password=password)
+
+
 # Tests of DashBoardPage view.
 
 
 def test_dashboard_page_parent_with_no_child(client, parent_user_password):
     """Test logging to dashboard page with no children added."""
     assert User.objects.count() == 1
-    username = 'tom_k'
-    password = 'password'
-    client.login(username=username, password=password)
+    user_logger(client, 'tom_k')
     response = client.get('/dashboard/')
     assert response.status_code == 200
     assert len(response.context['child_list']) == 0
@@ -27,9 +30,7 @@ def test_dashboard_page_parent_with_child(client, parent_user_password,
                                           child, alt_child):
     """Test logging to dashboard page with a child added.
     Confirms only child who's parent is logged in is in the child_list."""
-    username = 'tom_k'
-    password = 'password'
-    client.login(username=username, password=password)
+    user_logger(client, 'tom_k')
     response = client.get('/dashboard/')
     assert Child.objects.count() == 2
     assert response.status_code == 200
@@ -42,9 +43,7 @@ def test_dashboard_page_with_not_matching_user_child(client,
                                                      child):
     """Tests if get_queryset restricts children in child_list by checking
     parent."""
-    username = 'johny_c'
-    password = 'password'
-    client.login(username=username, password=password)
+    user_logger(client, 'johny_c')
     response = client.get('/dashboard/')
     assert response.status_code == 200
     # Since child has different parent it should not be added to the queryset.
@@ -54,12 +53,11 @@ def test_dashboard_page_with_not_matching_user_child(client,
 def test_dashboard_page_child_logged_in(client, child_user, child, alt_child):
     """Test logging to dashboard page as a child user. child_list should
     contain only the logged in child user."""
-    username = 'nat_k'
     password = 'password'
     user = child_user
     user.set_password(password)
     user.save()
-    client.login(username=username, password=password)
+    user_logger(client, 'nat_k')
     response = client.get('/dashboard/')
     assert Child.objects.count() == 2
     assert response.status_code == 200
@@ -70,13 +68,12 @@ def test_dashboard_page_child_logged_in(client, child_user, child, alt_child):
 # Tests of CreateChildPage view.
 
 def test_create_child_page_view(client, parent_user):
-    username = 'tom_k'
     password = 'password'
     user = parent_user
     user.set_password(password)
     user.save()
+    user_logger(client, 'tom_k')
     assert User.objects.count() == 1
-    client.login(username=username, password=password)
     response = client.get('/dashboard/add/child/')
     assert response.status_code == 200
     # Comfirm initial value for parent is passed using get_initial method.
@@ -91,11 +88,9 @@ def test_create_child_page_view(client, parent_user):
 def test_child_detail_view(client, child, parent_user_password):
     """Confirm view is properly displayed for a logged in parent user who's
     child we want to see."""
-    username = 'tom_k'
-    password = 'password'
+    user_logger(client, 'tom_k')
     assert User.objects.count() == 2
     assert Child.objects.count() == 1
-    client.login(username=username, password=password)
     response = client.get('/dashboard/child/detail/tom_k/nat_k/1')
     assert response.status_code == 200
     templates = response.templates
@@ -107,11 +102,9 @@ def test_child_detail_view_context_data(client, child, parent_user_password,
                                         oopsy_custom_description):
     """Confirm correct context data is set for the view if parent user is
     logged in."""
-    username = 'tom_k'
-    password = 'password'
+    user_logger(client, 'tom_k')
     assert User.objects.count() == 2
     assert Child.objects.count() == 1
-    client.login(username=username, password=password)
     response = client.get('/dashboard/child/detail/tom_k/nat_k/1')
     assert response.status_code == 200
     assert len(response.context['smileys']) == 1
@@ -125,11 +118,9 @@ def test_child_detail_view_child_logged_in(client, child_user_password,
                                            oopsy_custom_description):
     """Confirm correct context data is set for the view if child user is
     logged in."""
-    username = 'nat_k'
-    password = 'password'
+    user_logger(client, 'nat_k')
     assert User.objects.count() == 2
     assert Child.objects.count() == 1
-    client.login(username=username, password=password)
     response = client.get('/dashboard/child/detail/tom_k/nat_k/1')
     assert response.status_code == 200
     assert response.context['parent'] == 'tom_k'
@@ -143,9 +134,7 @@ def test_child_detail_view_test_func_parent(client, child,
                                             parent_user_password):
     """Test test_func when trying to access other user's child data when logged
     in as a parent."""
-    username = 'tom_k'
-    password = 'password'
-    client.login(username=username, password=password)
+    user_logger(client, 'tom_k')
     response = client.get('/dashboard/child/detail/jeffrey/gonzo/1')
     assert response.status_code == 302
 
@@ -154,8 +143,14 @@ def test_child_detail_view_test_func_child(client, child,
                                            child_user_password):
     """Test test_func when trying to access other child's data when logged in
     as a child."""
-    username = 'nat_k'
-    password = 'password'
-    client.login(username=username, password=password)
+    user_logger(client, 'nat_k')
     response = client.get('/dashboard/child/detail/jeffrey/gonzo/2')
     assert response.status_code == 302
+
+#
+# # Tests of AddAction view.
+#
+# def test_add_action_view_context(clinet, child, parent_user_password):
+#     """Confirm children of the parent user logged in get passed as context
+#     data."""
+
