fa8a8c99ee9eca142c03a77d764b548a34d1fe55
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-07-04 15:28:29 +0100

    Add favicons to site.
    Added favicon files and implemented them in <head> in base.html.
    Changed DEBUG in production to accept environtment variable.

--- Bnice/production_settings.py
+++ Bnice/production_settings.py
@@ -9,7 +9,7 @@ import dj_database_url
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=True, cast=bool)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
