a50e10343e138234c5325bdad10a24419ac1d500
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-07-05 20:43:58 +0100

    Add s3 settings in production_setttins.py.
    Added necessary configs in production_settings.py for Amazon s3 storage
    use.

--- Bnice/production_settings.py
+++ Bnice/production_settings.py
@@ -9,8 +9,7 @@ import dj_database_url
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+DEBUG = config('DEBUG', default=False, cast=bool)
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 ALLOWED_HOSTS = ['b-nice-staging.herokuapp.com',
@@ -21,3 +20,20 @@ DATABASES = {
     )
 }
 INSTALLED_APPS.append('storages')  # noqa
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'Bnice.storage_backends.MediaStorage'  # Media uploads
