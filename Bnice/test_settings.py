c9a8b3689b8d34655025f5452570885007b244db
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2018-10-31 20:48:27 +0000

    Consolidate settings into settings.py
    Removed old settings files.
    Adjusted travis.yml and tox.ini to match.

--- Bnice/test_settings.py
+++
@@ -1,9 +0,0 @@
-from Bnice.common_settings import *  # noqa
-import os
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa
-    }
-}
