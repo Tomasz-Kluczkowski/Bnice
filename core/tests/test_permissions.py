9afa37ef681431e08256c79840a91a96b90b0219
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2018-12-22 15:02:32 +0000

    Test GroupPermissionSetter.
    Added default_app_config for dashboard app (__init__.py).
    Added dashboard blobal permissions test.

--- core/tests/test_permissions.py
+++ core/tests/test_permissions.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group
 
 from accounts.apps import AccountsConfig
 from core.permissions import GroupPermissionSetter
+from dashboard.apps import DashboardConfig
 
 pytestmark = pytest.mark.django_db
 
@@ -15,7 +16,7 @@ class TestGroupPermissionSetter:
         self.group_perms_setter.add_groups(AccountsConfig)
         assert Group.objects.count() == len(GroupPermissionSetter.GROUPS)
 
-    def test_add_app_permissions(self, parent_user):
+    def test_add_accounts_app_permissions(self, parent_user):
         Group.objects.all().delete()
         self.group_perms_setter.add_groups(AccountsConfig)
         parents = Group.objects.get(name='Parents')
@@ -24,3 +25,13 @@ class TestGroupPermissionSetter:
         assert parents.permissions.count() == 2
         assert parent_user.has_perm('accounts.add_user_instance')
         assert parent_user.has_perm('accounts.add_child_instance')
+
+    def test_add_dashboard_app_permissions(self, parent_user):
+        Group.objects.all().delete()
+        self.group_perms_setter.add_groups(DashboardConfig)
+        parents = Group.objects.get(name='Parents')
+        parent_user.groups.add(parents)
+        self.group_perms_setter.add_app_permissions(DashboardConfig)
+        assert parents.permissions.count() == 2
+        assert parent_user.has_perm('dashboard.add_smiley_instance')
+        assert parent_user.has_perm('dashboard.add_oopsy_instance')
